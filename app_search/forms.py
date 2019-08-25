0910c33171526ce0036fc4d989e4cf57c97f48ae
Gaurav Tyagi <gauravtyagi777@gmail.com>
2019-08-25 14:25:46 -0700

    df

--- app_search/forms.py
+++ app_search/forms.py
@@ -4,10 +4,12 @@ class SearchForm(forms.Form):
 	fileName = forms.CharField(required=False, widget=forms.TextInput(attrs={
 			'id': 'search',
 			'name': 'fname',
-			'placeholder': 'File Name . .'
+			'placeholder': ' File Name . .',
+			'style':"height:38px;"
 		}))
 	content = forms.CharField(required=False, widget=forms.TextInput(attrs={
 			'id': 'search1',
 			'name': 'content',
-			'placeholder': 'Arguments . . '
+			'placeholder': '  Arguments . . ',
+			'style':"height:38px;"
 		}))
